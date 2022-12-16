dd9220eac9a7b516c16663335e9ce8c173f16da8
Ankura12 <75971744+Ankura12@users.noreply.github.com>
2022-12-16 16:31:59 +0530

    Update copy_stack.py

--- copy_stack.py
+++ copy_stack.py
@@ -2,24 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-
-
 inpu = int(input("Enter page number that you want to extract :"))
 stackoverflow=[]
 
 for hj in range(inpu):
     url = f'https://stackoverflow.com/users?page={hj}&tab=reputation&filter=week'
 
-    # url='https://stackoverflow.com/users'
     base_url='https://stackoverflow.com'
 
     r = requests.get(url)
     soup = BeautifulSoup(r.content,'html.parser')
 
     s = soup.find_all('div',class_="user-details")
-
-
 
     for i in s:
         u = i.find('a')
@@ -29,13 +23,9 @@ for hj in range(inpu):
         username = u.text
         U = u.get('href')
 
-        userlink = base_url+U
-
-        
+        userlink = base_url+U        
         w = requests.get(userlink)
         sou = BeautifulSoup(w.content,'html.parser')
-        
-
         reputation_num = sou.find('div',class_="fs-body3 fc-dark").text.strip()
 
         active_form = sou.find('div',class_="d-flex gs4 gsx ai-center").text.strip()
@@ -45,21 +35,7 @@ for hj in range(inpu):
         article_lates = sou.find('a',class_="question-hyperlink d-table tl-fixed w100 m0 ow-break-word js-gps-track").text.strip()
         
         top_meta_pos = sou.find('a',class_="question-hyperlink d-table tl-fixed w100 m0 ow-break-word js-gps-track").text.strip()
-
-        # collectives = sou.find('div',class_="s-card bar-md p0").text.strip()
-
-        # communities = sou.find('div',class_="s-card bar-md").text.strip()
-        
-        # try:
-        #     articlecount_link = userlink+'?tab=articles'
-        #     articlecount_w = requests.get(articlecount_link)
-        #     articlecount_soup = BeautifulSoup(articlecount_w.content,'html.parser')
-
-        #     article_count = articlecount_soup.find('div',class_="d-flex fd-column").text.strip()
-        # except:
-        #     article_count='None'
-
-    
+  
         stackoverflow_data={
             'Name':username.strip(),
             'url':userlink,
@@ -67,21 +43,11 @@ for hj in range(inpu):
             'Summary Reputation':reputation_num,
             'Active From':active_form,
             'Answers':answer_lates,
-            'Top Meta Posts':top_meta_pos,
-            # 'Articles Count':article_count,
-            # 'Communities':communities.strip()
+            'Top Meta Posts':top_meta_pos
         }
         stackoverflow.append(stackoverflow_data)
 
 
 df = pd.DataFrame(stackoverflow)
 
-df.to_csv('stack_overflow_data.csv')
-
-
-
-
-
-# https://stackoverflow.com/users/115145/commonsware?tab=articles
-
-# https://stackoverflow.com/users/115145/commonsware
+df.to_csv('stackoverflow data sample.csv')
